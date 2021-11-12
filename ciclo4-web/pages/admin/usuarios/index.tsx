108c467017a422aec7f187dec099e080defbdba9
AlexanderArias <90356937+Alexander-Arias-Arbelaez@users.noreply.github.com>
2021-11-12 17:40:08 -0500

    Agregando icono de home y títulos a las paginas

--- ciclo4-web/pages/admin/usuarios/index.tsx
+++ ciclo4-web/pages/admin/usuarios/index.tsx
@@ -1,15 +1,19 @@
+import Head from 'next/head';
 import Link from 'next/link'
 import React from 'react'
 
 const index = () => {
     return (
-        <div>
-            <div>Página de admin de usuarios</div>
-            <Link href='/'>
-                <a className='cursor-pointer'>Ir al home</a>
-            </Link>
-        </div>
-    )
+      <div>
+        <Head>
+          <title>Home | Admin Usuarios</title>
+        </Head>
+        <div>Página de admin de usuarios</div>
+        <Link href="/">
+          <a className="cursor-pointer">Ir al home</a>
+        </Link>
+      </div>
+    );
 }
 
 export default index
--- ciclo4-web/pages/index.tsx
+++ ciclo4-web/pages/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import Link from 'next/link';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-red-500'>
+    <div className='bg-blue-500'>
+      <Head>
+          <title>Home</title> 
+      </Head>
       <p>Bienvenido a mi pagina - prueba development</p>
       <Link href= '/admin/usuarios'>
           <a className='cursor-pointer'>Ir a admin usuarios</a>
       </Link>
+      <div>
+        <i className='fas fa-home'/>
+      </div>
     </div>
   )
 }
